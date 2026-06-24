9f9c2cc4fd4b97592f5c6009f1c193ef178bad63
Caio Felipe de Melo Barbosa <caio.barbosa@dataprev.gov.br>
2026-06-24 09:40:15 -0300

    feat(Login): starting to implement the login

---
+++ backend/src/main/java/com/ufrn/projeto/webApp/dto/UserLoginDTO.java
@@ -0,0 +1,19 @@
+package com.ufrn.projeto.webApp.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class UserLoginDTO {
+	
+	@NotBlank(message = "O email deve ser fornecido.")
+	private String email;
+	
+	@NotBlank(message = "A senha deve ser fornecida.")
+	private String password;
+
+}
---
+++ backend/src/main/java/com/ufrn/projeto/webApp/service/AuthService.java
@@ -0,0 +1,14 @@
+package com.ufrn.projeto.webApp.service;
+
+import org.springframework.stereotype.Service;
+
+import com.ufrn.projeto.webApp.dto.UserLoginDTO;
+
+@Service
+public class AuthService {
+	
+	public String login(UserLoginDTO dto) {
+		return null;
+	}
+
+}
