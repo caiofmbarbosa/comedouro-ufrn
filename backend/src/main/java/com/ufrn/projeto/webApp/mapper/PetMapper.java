9f2b9a463dc84eb51bae2a15928f86fe3791e7dc
Caio Felipe de Melo Barbosa <caio.barbosa@dataprev.gov.br>
2026-06-24 11:08:43 -0300

    refactor(PetMapper): removing non used method

--- backend/src/main/java/com/ufrn/projeto/webApp/mapper/PetMapper.java
+++ backend/src/main/java/com/ufrn/projeto/webApp/mapper/PetMapper.java
@@ -1,6 +1,5 @@
 package com.ufrn.projeto.webApp.mapper;
 
-import com.ufrn.projeto.webApp.dto.PetRequestUpdateDTO;
 import org.modelmapper.ModelMapper;
 
 import com.ufrn.projeto.webApp.dto.PetDTO;
@@ -8,7 +7,6 @@ import com.ufrn.projeto.webApp.dto.PetRequestDTO;
 import com.ufrn.projeto.webApp.entity.Pet;
 
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
 @NoArgsConstructor
 public class PetMapper {
@@ -24,17 +22,5 @@ public class PetMapper {
 		return mapper.map(pet, Pet.class);
 		
 	}
-
-    public Pet updatePet(PetRequestUpdateDTO petDTO, Pet pet) {
-        if (mapper.getTypeMap(PetRequestUpdateDTO.class, Pet.class) == null) {
-            mapper.createTypeMap(PetRequestUpdateDTO.class, Pet.class)
-                    .addMappings(mapper -> {
-                        mapper.skip(Pet::setId);
-                        mapper.skip(Pet::setTutor);
-                    });
-        }
-
-        return mapper.map(petDTO, pet);
-    }
 
 }
